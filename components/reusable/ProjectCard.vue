c889457dc4071edbd61fd7f0317d698fbede34cf
Vox Odinson <macos@MacOSs-MacBook-Air.local>
2025-06-01 01:13:31 +0700

    update project and skills

---
+++ components/reusable/ProjectCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <div
+        v-for="(p, idx) in data"
+        :key="idx"
+        class="w-full h-fit overflow-hidden">
+        <div 
+            class="w-full h-[300px] shadow-sm overflow-hidden rounded-md group relative">
+            <img 
+                :src="p.img" 
+                alt="img"
+                class="w-full h-full object-cover">
+            <ULink 
+                to="#"
+                class="hidden group-hover:flex cursor-pointer bg-white bg-opacity-45 items-center justify-center transition w-full h-full absolute top-0 left-0">
+                <div
+                    class="w-[70px] h-[70px] flex justify-center items-center rounded-full bg-gray-700">
+                    <UIcon
+                        name="iconamoon:arrow-top-right-1"
+                        class="w-12 h-12 text-white"/>
+                </div>
+            </ULink>
+        </div>
+        <h3
+            class="my-2 font-semibold text-gray-800 text-wrap">
+            {{ p.title }}
+        </h3>
+        <p
+            class="text-gray-400 text-[.8rem] line-clamp-2 overflow-hidden text-ellipsis w-full">
+            {{ p.detail }}
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+/**
+* Begin::Set event trigger to parent component
+*/
+const route = useRoute();
+withDefaults(defineProps<{
+    data: any,
+}>(),{
+    data: () => []
+});
+/**
+* End::Set event trigger to parent component
+*/
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/ui/Skills.vue
@@ -0,0 +1,26 @@
+<template>
+    <div
+        class="py-12 w-full flex items-start justify-start flex-col">
+        <h3 class="text-3xl font-bold text-gray-800 dark:text-white mt-24">
+            Skills
+        </h3>
+        <div 
+            class="w-full flex flex-wrap items-center justify-center mt-6 gap-y-6">
+            <div 
+                v-for="(skill, idx) in 20"
+                class="min-w-[120px] w-[10%] h-[100px] flex items-center justify-center">
+                <UIcon
+                    name="vscode-icons:file-type-tailwind"
+                    class="w-20 h-20"/>
+                <h6
+                    class="text-1xl">
+                    Nuxt JS
+                </h6>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>   
